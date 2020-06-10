35cd3972f877c77fcfdd5b6b8ed2b885af9846f8
walid <walid.bouguima@gmail.com>
2020-06-10 14:06:22 +0200

    Find anothor way to toggle instead of if else statment use .toggle on the class list directly.

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links'); 
 
 
-navToggle.addEventListener('click', function () {
+navToggle.addEventListener('click',  () => {
 
-    if(links.classList.contains('show-links')){
+   /* if(links.classList.contains('show-links')){
         links.classList.remove('show-links'); 
     }else{
         links.classList.add("show-links")
-    }
+    }*/
+
+    links.classList.toggle('show-links');
 })
